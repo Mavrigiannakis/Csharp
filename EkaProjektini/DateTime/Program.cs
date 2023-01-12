0cfb46c436fe17a3da51056f3a73a06a566b79d8
Mavrigiannakis <111268745+Mavrigiannakis@users.noreply.github.com>
2023-01-12 11:35:15 +0200

    ekat

---
+++ EkaProjektini/DateTime/Program.cs
@@ -0,0 +1,8 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DateTime now = DateTime.Now;
+        Console.WriteLine(now.ToString("F", new System.Globalization.CultureInfo("fi-FI")));
+    }
+}
---
+++ EkaProjektini/Math/Program.cs
@@ -0,0 +1,9 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int luku = 50;
+        float neljojuuri = (float)Math.Sqrt(luku);
+        Console.WriteLine("Neljiöjuuri luvusta {0} on {1} ", luku, neljojuuri.ToString());
+    }
+}
