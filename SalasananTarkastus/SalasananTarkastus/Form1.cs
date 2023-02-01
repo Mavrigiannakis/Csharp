e9b4e920b5d0cc67e52a73688bb0f163754eccf2
Stylianos Mavrigiannakis <stylianos.mavrigiann@edu.keuda.fi>
2023-02-01 14:59:34 +0200

    tehtava6

---
+++ SalasananTarkastus/SalasananTarkastus/Form1.cs
@@ -0,0 +1,24 @@
+namespace SalasananTarkastus
+{
+    public partial class SalasanaForm : Form
+    {
+        public SalasanaForm()
+        {
+            InitializeComponent();
+        }
+
+        private void TarkistaBT_Click(object sender, EventArgs e)
+        {
+            if (KayttajaTB.Text == "Stelios" && SalasanaTB.Text == "souvlakiNamNam")
+            {
+                SalasanaPanel.Visible = false;
+                SalasanaOikeinPanel.Visible = true;
+            }
+            else
+            {
+                VirheviestiLB.Text = "Käyttäjätunnus tai salasana virheellinen!";
+                VirheviestiLB.Visible= true;
+            }
+        }
+    }
+}
