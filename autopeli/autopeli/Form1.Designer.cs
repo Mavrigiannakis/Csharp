a0b73a31f1377658cb291dfba4df26ed88bc1569
Stylianos Mavrigiannakis <stylianos.mavrigiann@edu.keuda.fi>
2023-04-19 11:54:55 +0300

    auto peli c#

---
+++ autopeli/autopeli/Form1.Designer.cs
@@ -0,0 +1,296 @@
+﻿namespace autopeli
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.viiva1 = new System.Windows.Forms.PictureBox();
+            this.viiva2 = new System.Windows.Forms.PictureBox();
+            this.viiva4 = new System.Windows.Forms.PictureBox();
+            this.viiva3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.ajastin = new System.Windows.Forms.Timer(this.components);
+            this.oranssiauto = new System.Windows.Forms.PictureBox();
+            this.viholinen1 = new System.Windows.Forms.PictureBox();
+            this.viholinen2 = new System.Windows.Forms.PictureBox();
+            this.viholinen3 = new System.Windows.Forms.PictureBox();
+            this.gameoverLB = new System.Windows.Forms.Label();
+            this.koliko4 = new System.Windows.Forms.PictureBox();
+            this.koliko1 = new System.Windows.Forms.PictureBox();
+            this.koliko3 = new System.Windows.Forms.PictureBox();
+            this.koliko2 = new System.Windows.Forms.PictureBox();
+            this.kolikotLB = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.viiva1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.viiva2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.viiva4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.viiva3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.oranssiauto)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.viholinen1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.viholinen2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.viholinen3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.koliko4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.koliko1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.koliko3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.koliko2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // viiva1
+            // 
+            this.viiva1.BackColor = System.Drawing.Color.White;
+            this.viiva1.Location = new System.Drawing.Point(175, -3);
+            this.viiva1.Name = "viiva1";
+            this.viiva1.Size = new System.Drawing.Size(10, 97);
+            this.viiva1.TabIndex = 0;
+            this.viiva1.TabStop = false;
+            // 
+            // viiva2
+            // 
+            this.viiva2.BackColor = System.Drawing.Color.White;
+            this.viiva2.Location = new System.Drawing.Point(175, 110);
+            this.viiva2.Name = "viiva2";
+            this.viiva2.Size = new System.Drawing.Size(10, 108);
+            this.viiva2.TabIndex = 1;
+            this.viiva2.TabStop = false;
+            // 
+            // viiva4
+            // 
+            this.viiva4.BackColor = System.Drawing.Color.White;
+            this.viiva4.Location = new System.Drawing.Point(175, 368);
+            this.viiva4.Name = "viiva4";
+            this.viiva4.Size = new System.Drawing.Size(10, 110);
+            this.viiva4.TabIndex = 2;
+            this.viiva4.TabStop = false;
+            // 
+            // viiva3
+            // 
+            this.viiva3.BackColor = System.Drawing.Color.White;
+            this.viiva3.Location = new System.Drawing.Point(175, 241);
+            this.viiva3.Name = "viiva3";
+            this.viiva3.Size = new System.Drawing.Size(10, 104);
+            this.viiva3.TabIndex = 3;
+            this.viiva3.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.BackColor = System.Drawing.Color.White;
+            this.pictureBox5.Location = new System.Drawing.Point(-4, -3);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(10, 464);
+            this.pictureBox5.TabIndex = 4;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.BackColor = System.Drawing.Color.White;
+            this.pictureBox6.Location = new System.Drawing.Point(375, -3);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(10, 464);
+            this.pictureBox6.TabIndex = 5;
+            this.pictureBox6.TabStop = false;
+            // 
+            // ajastin
+            // 
+            this.ajastin.Enabled = true;
+            this.ajastin.Interval = 10;
+            this.ajastin.Tick += new System.EventHandler(this.ajastin_Tick);
+            // 
+            // oranssiauto
+            // 
+            this.oranssiauto.Image = ((System.Drawing.Image)(resources.GetObject("oranssiauto.Image")));
+            this.oranssiauto.Location = new System.Drawing.Point(102, 277);
+            this.oranssiauto.Name = "oranssiauto";
+            this.oranssiauto.Size = new System.Drawing.Size(34, 68);
+            this.oranssiauto.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.oranssiauto.TabIndex = 6;
+            this.oranssiauto.TabStop = false;
+            // 
+            // viholinen1
+            // 
+            this.viholinen1.Image = ((System.Drawing.Image)(resources.GetObject("viholinen1.Image")));
+            this.viholinen1.Location = new System.Drawing.Point(36, 123);
+            this.viholinen1.Name = "viholinen1";
+            this.viholinen1.Size = new System.Drawing.Size(34, 68);
+            this.viholinen1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.viholinen1.TabIndex = 7;
+            this.viholinen1.TabStop = false;
+            // 
+            // viholinen2
+            // 
+            this.viholinen2.Image = ((System.Drawing.Image)(resources.GetObject("viholinen2.Image")));
+            this.viholinen2.Location = new System.Drawing.Point(230, 67);
+            this.viholinen2.Name = "viholinen2";
+            this.viholinen2.Size = new System.Drawing.Size(35, 68);
+            this.viholinen2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.viholinen2.TabIndex = 8;
+            this.viholinen2.TabStop = false;
+            // 
+            // viholinen3
+            // 
+            this.viholinen3.BackColor = System.Drawing.Color.Gray;
+            this.viholinen3.Image = ((System.Drawing.Image)(resources.GetObject("viholinen3.Image")));
+            this.viholinen3.Location = new System.Drawing.Point(304, 187);
+            this.viholinen3.Name = "viholinen3";
+            this.viholinen3.Size = new System.Drawing.Size(34, 68);
+            this.viholinen3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.viholinen3.TabIndex = 9;
+            this.viholinen3.TabStop = false;
+            // 
+            // gameoverLB
+            // 
+            this.gameoverLB.AutoSize = true;
+            this.gameoverLB.BackColor = System.Drawing.Color.BurlyWood;
+            this.gameoverLB.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gameoverLB.ForeColor = System.Drawing.Color.Red;
+            this.gameoverLB.Location = new System.Drawing.Point(96, 110);
+            this.gameoverLB.Name = "gameoverLB";
+            this.gameoverLB.Size = new System.Drawing.Size(186, 31);
+            this.gameoverLB.TabIndex = 10;
+            this.gameoverLB.Text = "GAME OVER";
+            // 
+            // koliko4
+            // 
+            this.koliko4.Image = ((System.Drawing.Image)(resources.GetObject("koliko4.Image")));
+            this.koliko4.Location = new System.Drawing.Point(304, 302);
+            this.koliko4.Name = "koliko4";
+            this.koliko4.Size = new System.Drawing.Size(34, 34);
+            this.koliko4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.koliko4.TabIndex = 11;
+            this.koliko4.TabStop = false;
+            // 
+            // koliko1
+            // 
+            this.koliko1.Image = ((System.Drawing.Image)(resources.GetObject("koliko1.Image")));
+            this.koliko1.Location = new System.Drawing.Point(94, 38);
+            this.koliko1.Name = "koliko1";
+            this.koliko1.Size = new System.Drawing.Size(34, 34);
+            this.koliko1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.koliko1.TabIndex = 12;
+            this.koliko1.TabStop = false;
+            // 
+            // koliko3
+            // 
+            this.koliko3.Image = ((System.Drawing.Image)(resources.GetObject("koliko3.Image")));
+            this.koliko3.Location = new System.Drawing.Point(89, 207);
+            this.koliko3.Name = "koliko3";
+            this.koliko3.Size = new System.Drawing.Size(34, 34);
+            this.koliko3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.koliko3.TabIndex = 13;
+            this.koliko3.TabStop = false;
+            // 
+            // koliko2
+            // 
+            this.koliko2.Image = ((System.Drawing.Image)(resources.GetObject("koliko2.Image")));
+            this.koliko2.Location = new System.Drawing.Point(304, 87);
+            this.koliko2.Name = "koliko2";
+            this.koliko2.Size = new System.Drawing.Size(34, 34);
+            this.koliko2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.koliko2.TabIndex = 14;
+            this.koliko2.TabStop = false;
+            // 
+            // kolikotLB
+            // 
+            this.kolikotLB.AutoSize = true;
+            this.kolikotLB.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.kolikotLB.ForeColor = System.Drawing.Color.Gold;
+            this.kolikotLB.Location = new System.Drawing.Point(12, 9);
+            this.kolikotLB.Name = "kolikotLB";
+            this.kolikotLB.Size = new System.Drawing.Size(83, 20);
+            this.kolikotLB.TabIndex = 15;
+            this.kolikotLB.Text = "Kolikot=0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gray;
+            this.ClientSize = new System.Drawing.Size(384, 461);
+            this.Controls.Add(this.kolikotLB);
+            this.Controls.Add(this.koliko2);
+            this.Controls.Add(this.koliko3);
+            this.Controls.Add(this.koliko1);
+            this.Controls.Add(this.koliko4);
+            this.Controls.Add(this.gameoverLB);
+            this.Controls.Add(this.viholinen3);
+            this.Controls.Add(this.viholinen2);
+            this.Controls.Add(this.viholinen1);
+            this.Controls.Add(this.oranssiauto);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.viiva3);
+            this.Controls.Add(this.viiva4);
+            this.Controls.Add(this.viiva2);
+            this.Controls.Add(this.viiva1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.viiva1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.viiva2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.viiva4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.viiva3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.oranssiauto)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.viholinen1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.viholinen2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.viholinen3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.koliko4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.koliko1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.koliko3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.koliko2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox viiva1;
+        private System.Windows.Forms.PictureBox viiva2;
+        private System.Windows.Forms.PictureBox viiva4;
+        private System.Windows.Forms.PictureBox viiva3;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.Timer ajastin;
+        private System.Windows.Forms.PictureBox oranssiauto;
+        private System.Windows.Forms.PictureBox viholinen1;
+        private System.Windows.Forms.PictureBox viholinen2;
+        private System.Windows.Forms.PictureBox viholinen3;
+        private System.Windows.Forms.Label gameoverLB;
+        private System.Windows.Forms.PictureBox koliko4;
+        private System.Windows.Forms.PictureBox koliko1;
+        private System.Windows.Forms.PictureBox koliko3;
+        private System.Windows.Forms.PictureBox koliko2;
+        private System.Windows.Forms.Label kolikotLB;
+    }
+}
+
---
+++ autopeli/autopeli/Form1.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace autopeli
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            gameoverLB.Visible=false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void ajastin_Tick(object sender, EventArgs e)
+        {
+            moveline(gamespeed);
+            viholinen(gamespeed);
+            gameover();
+            koliko(gamespeed);
+            kolikotcollection();
+        }
+
+        int collectedkoliko = 0;
+
+        Random r = new Random();
+        int x, y;
+        void viholinen(int speed)
+        {
+            if (viholinen1.Top >=500)
+            {
+                x = r.Next(0, 200);
+                viholinen1.Location = new Point(x, 0);
+            }
+            else
+            {
+                viholinen1.Top += speed;
+            }
+            if (viholinen2.Top >=500)
+            {
+                x = r.Next(0, 400);
+                viholinen2.Location = new Point(x, 0);
+            }
+            else
+            {
+                viholinen2.Top += speed;
+            }
+            if (viholinen3.Top >=500)
+            {
+                x = r.Next(200, 350);
+                viholinen3.Location = new Point(x, 0);
+            }
+            else
+            {
+                viholinen3.Top += speed;
+            }
+
+        }
+
+        void koliko(int speed)
+        {
+            if (koliko1.Top >=500)
+            {
+                x = r.Next(0, 200);
+                koliko1.Location = new Point(x, 0);
+            }
+            else
+            {
+                koliko1.Top += speed;
+            }
+            if (koliko2.Top >=500)
+            {
+                x = r.Next(0, 200);
+                koliko2.Location = new Point(x, 0);
+            }
+            else
+            {
+                koliko2.Top += speed;
+            }
+            if (koliko3.Top >=500)
+            {
+                x = r.Next(50, 300);
+                koliko3.Location = new Point(x, 0);
+            }
+            else
+            {
+                koliko3.Top += speed;
+            }
+            if (koliko4.Top >=500)
+            {
+                x = r.Next(0, 200);
+                koliko4.Location = new Point(x, 0);
+            }
+            else
+            {
+                koliko4.Top += speed;
+            }
+
+        }
+    void gameover()
+        {
+            if(oranssiauto.Bounds.IntersectsWith(viholinen1.Bounds))
+                {
+                ajastin.Enabled=false;
+                gameoverLB.Visible=true;
+                }
+            if (oranssiauto.Bounds.IntersectsWith(viholinen2.Bounds))
+            {
+                ajastin.Enabled=false;
+                gameoverLB.Visible=true;
+            }
+            if (oranssiauto.Bounds.IntersectsWith(viholinen3.Bounds))
+            {
+                ajastin.Enabled=false;
+                gameoverLB.Visible=true;
+            }
+        }
+        void moveline(int speed)
+        {
+            if (viiva1.Top >= 500)
+            {
+                viiva1.Top = 0;
+            }
+            else
+            {
+                viiva1.Top += speed;
+            }
+
+            if (viiva2.Top >= 500)
+            {
+                viiva2.Top = 0;
+            }
+            else
+            {
+                viiva2.Top += speed;
+            }
+
+            if (viiva3.Top >= 500)
+            {
+                viiva3.Top = 0;
+            }
+            else
+            {
+                viiva3.Top += speed;
+            }
+
+            if (viiva4.Top >= 500)
+            {
+                viiva4.Top = 0;
+            }
+            else
+            {
+                viiva4.Top += speed;
+            }
+
+        }
+
+        void kolikotcollection()
+        {
+             if (oranssiauto.Bounds.IntersectsWith(koliko1.Bounds))
+                {
+                    collectedkoliko++;
+                    kolikotLB.Text = "Kolikot=" + collectedkoliko.ToString();
+                    koliko1.Location = new Point(x, 0);
+                }
+            if (oranssiauto.Bounds.IntersectsWith(koliko2.Bounds))
+                {
+                    collectedkoliko++;
+                    kolikotLB.Text = "Kolikot=" + collectedkoliko.ToString();
+                    koliko2.Location = new Point(x, 0);
+                }
+            if (oranssiauto.Bounds.IntersectsWith(koliko3.Bounds))
+                {
+                    collectedkoliko++;
+                    kolikotLB.Text = "Kolikot=" + collectedkoliko.ToString();
+                    koliko3.Location = new Point(x, 0);
+                }
+            if (oranssiauto.Bounds.IntersectsWith(koliko4.Bounds))
+                {
+                    collectedkoliko++;
+                    kolikotLB.Text = "Kolikot=" + collectedkoliko.ToString();
+                    koliko4.Location = new Point(x, 0);
+            }
+        }
+
+        int gamespeed = 0;
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+                {
+                    if(oranssiauto.Left>0)
+                    oranssiauto.Left += -8;
+                }
+            if (e.KeyCode == Keys.Right)
+                {
+                    if(oranssiauto.Right<380)
+                    oranssiauto.Left += 8;
+                }
+            if (e.KeyCode == Keys.Up)
+            {
+                if (gamespeed < 21)
+                {
+                    gamespeed++;
+                }    
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                if (gamespeed < 0)
+                {
+                    gamespeed++;
+                }
+            }
+        }
+    }
+}
